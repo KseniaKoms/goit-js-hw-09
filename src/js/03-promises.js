6256137d80e90e96a0291fe4177b7e0cb2026623
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-05-15 13:50:12 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,40 +12,35 @@ function onFormSubmit(e) {
   let position = 1;
 
   setTimeout(() => {
-    createPromise(position, delay)            
+      createPromise(position, delay)            
       .then(({ position, delay }) => {
          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-
-   const intervalId =  setInterval(() => {
-     position += 1;
-      if (position <= amount) {
-       delay = parseInt(delay) + parseInt(step);
-       createPromise(position, delay)            
-         .then(({ position, delay }) => {
-           Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-         })
-         .catch(({ position, delay }) => {
-           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-         });
-      } else {
-       clearInterval(intervalId)
-      } 
-   }, step)
+    for (let position = 2; position <= amount; position += 1) {
+       delay = parseInt(delay) + parseInt(step)
+         createPromise(position, delay)            
+      .then(({ position, delay }) => {
+         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    }
  }, delay) 
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
       const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+    setTimeout(() => {
+    if (shouldResolve) {
     resolve({position, delay});
   } else {
     reject({position, delay});
   }
-  })
+  }, delay)
+})
 }
-
